95ca0126fe69d9bf914b9db0a5797015e0345271
sub7x22 <panguins1610@hotmail.com>
2021-01-25 23:45:58 -0500

    Added SqlCommandAPIRepo and replace mockcommanderRepo with it. Also implemented in startup

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -3,16 +3,25 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+            (Configuration.GetConnectionString("PostgreSqlConnection")));
             //SECTION 1. Add code below
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
         public void Configure(IApplicationBuilder app,
         IWebHostEnvironment env)
